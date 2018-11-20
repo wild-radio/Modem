4c7e8224186f63d8978ad7006bf7492f5f2ef750
killertux <bruno.canguru@hotmail.com>
2018-11-20 17:41:46 -0200

    Nasd

--- src/WRCP/WRCPReceiver.hpp
+++ src/WRCP/WRCPReceiver.hpp
@@ -1,7 +1,7 @@
 #ifndef MODEM_WRCPRECEIVER_HPP
 #define MODEM_WRCPRECEIVER_HPP
 
-static const int MAX_RECEIVED_PACKETS_BUFFER = 6;
+static const int MAX_RECEIVED_PACKETS_BUFFER = 15;
 
 #include <vector>
 #include "../ThreadInterface.hpp"
