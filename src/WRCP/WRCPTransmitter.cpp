7f27b033f7f5ffb7a7eb4c25ee28742d4986dd41
killertux <bruno.canguru@hotmail.com>
2018-11-21 00:05:29 -0200

    Almost there

--- src/WRCP/WRCPTransmitter.cpp
+++ src/WRCP/WRCPTransmitter.cpp
@@ -7,7 +7,7 @@
 void WRCPTransmitter::run() {
 
 	int number = 0;
-	unsigned char data[20];
+	unsigned char data[30];
 	while (true) {
 		if (!this->outcoming_packets->hasMessage()) {
 			delay(300);
@@ -15,10 +15,10 @@ void WRCPTransmitter::run() {
 		}
 
 		WRCP packet = this->outcoming_packets->pull();
-		bzero(data, 20);
+		bzero(data, 30);
 		unsigned char *wrcp_data = packet.getData();
-		memcpy(data + 5, wrcp_data, (size_t)WRCP_PACKET_SIZE);
-		modem.writeData(wrcp_data, 20);
+		memcpy(data + 10, wrcp_data, (size_t)WRCP_PACKET_SIZE);
+		modem.writeData(wrcp_data, 30);
 
 		if (number == 255)
 			number = 0;
