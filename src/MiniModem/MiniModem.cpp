57bc19222a315c76ca3a0d1ba3e415dcf545d7d0
killertux <bruno.canguru@hotmail.com>
2018-11-22 22:39:30 -0200

    Trying to fix problem in the reciver

--- src/MiniModem/MiniModem.cpp
+++ src/MiniModem/MiniModem.cpp
@@ -17,7 +17,7 @@ void MiniModem::writeData(const unsigned char *data, int size) {
 }
 
 int MiniModem::readData(unsigned char *data, int size) {
-	if (read_stream == nullptr || feof(this->read_stream)) {
+	if (read_stream == nullptr) {
 		read_stream = popen((RX_COMMAND + " 2>/dev/null").c_str(), "r");
 	}
 
