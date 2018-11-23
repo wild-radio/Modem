dff14bbbf7c0511708467dfc69a5e3b0da95cd52
killertux <bruno.canguru@hotmail.com>
2018-11-22 23:46:13 -0200

    Trying to fix problem in the reciver

--- src/MiniModem/MiniModem.cpp
+++ src/MiniModem/MiniModem.cpp
@@ -7,13 +7,13 @@ void MiniModem::writeData(const unsigned char *data, int size) {
 	std::string command = TX_COMMAND;
 
 	this->ptt = PTTResolver::resolve();
-
+	this->write_stream = popen(command.c_str(), "w");
+	
 	if (write_stream == nullptr) {
 		throw StreamException(1, "Failed to open the write_stream!");
 	}
 
 	this->ptt->push();
-	this->write_stream = popen(command.c_str(), "w");
 	fwrite(data, sizeof(char), (size_t) size, write_stream);
 	pclose(write_stream);
 	this->ptt->release();
