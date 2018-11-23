82e85fbc01298fbd42cfe3f7e96b2b7bb936f2e3
killertux <bruno.canguru@hotmail.com>
2018-11-23 00:36:41 -0200

    Trying to fix problem in the reciver

--- src/MiniModem/MiniModem.hpp
+++ src/MiniModem/MiniModem.hpp
@@ -3,6 +3,7 @@
 
 #include <cstdio>
 #include <string>
+#include <mutex>
 #include "../ModemInterface.hpp"
 #include "PTT.hpp"
 #include "PTTResolver.hpp"
@@ -10,9 +11,13 @@
 class MiniModem : public ModemInterface {
 public:
 	MiniModem() {
-		this->read_stream = popen((RX_COMMAND + " 2>/dev/null").c_str(), "r");;
+		this->read_stream = openReadStream();
 		this->write_stream = nullptr;
+		this->last_start_timestamp = this->getTimestamp();
 	}
+
+	FILE *openReadStream() const { return popen((RX_COMMAND + " 2>/dev/null").c_str(), "r"); }
+
 	void writeData(const unsigned char *data, int size) override;
 	int readData(unsigned char *data, int size) override;
 
@@ -27,6 +32,13 @@ private:
 	std::string alsa_option;
 	std::string TX_COMMAND = "minimodem -R 44100 --tx 300";
 	std::string RX_COMMAND = "minimodem -R 44100 --rx 300 -c 1";
+	std::mutex mutex;
+
+	void checkForRestart();
+
+	int getTimestamp();
+
+	int last_start_timestamp;
 };
 
 
